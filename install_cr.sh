004e089a59d1006f685f13b39f223d6817bc71fd
Mikołaj Komar <mikolaj.komar@intel.com>
2025-02-28 15:25:04 +0000

    x

--- install_cr.sh
+++ install_cr.sh
@@ -1,8 +1,8 @@
 
-python3 -m venv venv/urbench. 
+python3 -m venv venv/urbench
 
 . venv/urbench/bin/activate
 
-pip install -r llvm/unified-runtime/third_party/benchmark_requirements.txt
-cd llvm/unified-runtime/scripts/benchmarks/
-./main.py --dry-run --compute-runtime HEAD --build-igc ../../compute-runtime
+# pip install -r llvm/unified-runtime/third_party/benchmark_requirements.txt
+# cd llvm/unified-runtime/scripts/benchmarks/
+# ./main.py --dry-run --compute-runtime HEAD --build-igc ../../compute-runtime
