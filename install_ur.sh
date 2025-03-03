a7fcd82634d6f673f7f37f9cd2efa0870d00c53f
Mikołaj Komar <mikolaj.komar@intel.com>
2025-03-03 14:22:24 +0000

    fix install_ur script

--- install_ur.sh
+++ install_ur.sh
@@ -1,11 +1,14 @@
 
 cd llvm
+export BTYPE=Release
+
+. venv/urbench/bin/activate
 
 cmake -DCMAKE_BUILD_TYPE=$BTYPE \
   -S$HOME/mkomar/KVM-utils/llvm/unified-runtime \
   -B$HOME/mkomar/KVM-utils/llvm/unified-runtime/build_$BTYPE \
-  -DCMAKE_C_COMPILER=$C \
-  -DCMAKE_CXX_COMPILER=$CXX \
+  -DCMAKE_C_COMPILER=gcc \
+  -DCMAKE_CXX_COMPILER=g++ \
   -DUR_DEVELOPER_MODE=ON \
   -DUR_BUILD_TESTS=ON \
   -DUR_CONFORMANCE_TEST_LOADER=OFF \
@@ -13,9 +16,7 @@ cmake -DCMAKE_BUILD_TYPE=$BTYPE \
   -DUR_BUILD_ADAPTER_L0_V2=ON \
   -DUMF_DISABLE_HWLOC=OFF \
   -DUR_FORMAT_CPP_STYLE=ON \
-  -DUR_LEVEL_ZERO_LOADER_LIBRARY=$HOME/mkomar/compute-runtime/level-zero-build/lib/libze_loader.so \
-  -DUR_LEVEL_ZERO_INCLUDE_DIR=$HOME/mkomar/compute-runtime/level-zero-repo/include \
   -DUR_DPCXX=$HOME/dpcpp_compiler/bin/clang++ \
   -DUR_SYCL_LIBRARY_DIR=$HOME/dpcpp_compiler/lib
  
-cmake --build $HOME/local/l0/llvm/unified-runtime/build_$BTYPE -- -j
+cmake --build $HOME/mkomar/KVM-utils/llvm/unified-runtime/build_$BTYPE -- -j
